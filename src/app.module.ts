157a8c9ddf7589cc0196e69c0dfdec273a663bfe
Thanh Nguyen <vnscriptkid@gmail.com>
2022-02-21 19:33:50 +0700

    feat: connect to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ExamsModule } from './exams/exams.module';
+import { RoomBookingsModule } from './room-bookings/room-bookings.module';
+import { ExamSchedulesModule } from './exam-schedules/exam-schedules.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3305,
+      username: 'root',
+      password: '123456',
+      database: 'exams',
+      synchronize: true,
+    }),
+    ExamsModule,
+    RoomBookingsModule,
+    ExamSchedulesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
