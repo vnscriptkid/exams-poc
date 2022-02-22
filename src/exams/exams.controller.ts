91d6e473fc9fb26927f4a9a137c7f648d39042c8
Thanh Nguyen <vnscriptkid@gmail.com>
2022-02-22 23:51:15 +0700

    feat(exam): findOneWithScheduling

--- src/exams/exams.controller.ts
+++ src/exams/exams.controller.ts
@@ -1,5 +1,7 @@
+import { ExamDto } from './dtos/exam.dto';
 import { Controller, Get, Param } from '@nestjs/common';
 import { CurrentUser } from 'src/auth/current-user.decorator';
+import { Serialize } from 'src/common/interceptors/serialize.interceptor';
 import { ExamsService } from './exams.service';
 
 @Controller('exams')
@@ -7,14 +9,123 @@ export class ExamsController {
   constructor(private readonly examService: ExamsService) {}
 
   @Get(':id')
-  getOneExam(@Param('id') id: number) {
-    return this.examService.findById(id);
+  @Serialize(ExamDto)
+  getOneExam(@Param('id') examId: number, @CurrentUser() user) {
+    return this.examService.findOneWithScheduling(examId, user.organizationId);
   }
 
   @Get()
   async getExams(@CurrentUser() user: any) {
-    return await this.examService
+    const exams = await this.examService
       .findAllByOrgQuery(user.organizationId)
       .getMany();
+
+    // const examIds = exams.map((e) => (e.dataValues || e).id);
+
+    // const { studentRegisteredExams, studentAllocatedExams } =
+    //   await this.calculateRegisteredAndAllocated(exams, examIds, {
+    //     organizationId,
+    //   });
+
+    // const allExams = exams;
+
+    // let builtExams = allExams.map((e) => {
+    //   const {
+    //     Y _linked_from_ids,
+    //     Y _group_ids,
+    //     _child_scheduled_students,
+    //     _publication_statuses,
+    //     _is_group,
+    //     _is_fully_scheduled,
+    //     start_seat_no: scheduled_start_seat_no,
+    //     end_seat_no: scheduled_end_seat_no,
+    //     start_time: scheduled_start_time,
+    //     end_time: scheduled_end_time,
+    //     room_id: scheduled_room_id,
+    //     exam_slot_id: scheduled_exam_slot_id,
+    //     room_booking_id: scheduled_room_booking_id,
+    //     room_booking_date: exam_target_date,
+    //     checklist = [],
+    //     ...restExamInfo
+    //   } = e;
+
+    //   const { publication_status } = restExamInfo;
+
+    //   let childrenIds;
+    //   let arrPuplicationStatuses = [publication_status];
+    //   const linkedFromExamIds = _linked_from_ids
+    //     ? _linked_from_ids.split(',').map((_id) => parseInt(_id, 10))
+    //     : [];
+
+    //   if (_is_group) {
+    //     childrenIds = _group_ids
+    //       ? _group_ids.split(',').map((_id) => parseInt(_id, 10))
+    //       : [];
+    //     arrPuplicationStatuses = (_publication_statuses || '').split(',');
+
+    //     arrPuplicationStatuses.shift();
+    //     childrenIds.shift();
+    //   } else {
+    //     childrenIds = [];
+    //   }
+
+    //   let publicationStatus;
+    //   if (!_is_group) {
+    //     publicationStatus = publication_status;
+    //   } else {
+    //     publicationStatus =
+    //       _.find(
+    //         arrPuplicationStatuses,
+    //         (status) => status === examStatus.NOT_PUBLISHED
+    //       ) ||
+    //       _.find(
+    //         arrPuplicationStatuses,
+    //         (status) => status === examStatus.PUBLISHED_WITH_CHANGES
+    //       ) ||
+    //       _.find(
+    //         arrPuplicationStatuses,
+    //         (status) => status === examStatus.DEPUBLISHED
+    //       ) ||
+    //       _.find(
+    //         arrPuplicationStatuses,
+    //         (status) => status === examStatus.PUBLISHED
+    //       ) ||
+    //       publication_status;
+    //   }
+
+    //   const registeredStudents = studentRegisteredExams[e.id] || 0;
+    //   const allocatedStudents = studentAllocatedExams[e.id] || 0;
+
+    //   return {
+    //     ...restExamInfo,
+    //     checklist,
+    //     publication_status: publicationStatus,
+    //     students_registered: registeredStudents,
+    //     students_allocated: allocatedStudents,
+
+    //     linked_from_exam_ids: linkedFromExamIds,
+    //     is_scheduled: _is_fully_scheduled,
+    //     scheduled_start_time,
+    //     scheduled_end_time,
+    //     scheduled_room_id,
+    //     scheduled_exam_slot_id,
+    //     scheduled_room_booking_id,
+    //     scheduled_end_seat_no,
+    //     scheduled_start_seat_no,
+    //     exam_target_date,
+    //     '@children': childrenIds,
+    //   };
+    // });
+
+    // if (!withChildren) {
+    //   const examIds = (exams || []).map((e) => e.id);
+    //   builtExams = builtExams.filter((e) => examIds.includes(e.id));
+    // }
+
+    // const uniqData = _.uniqBy(builtExams, (e) => e.id);
+
+    // return uniqData;
+
+    return exams;
   }
 }
--- src/exams/exams.service.ts
+++ src/exams/exams.service.ts
@@ -42,11 +42,30 @@ export class ExamsService {
     return this.examRepository
       .createQueryBuilder('e')
       .where('e.organization_id = :organizationId', { organizationId })
-      .andWhere('e.parent is NULL') // parent exam || single exam
+      .andWhere('e.parent_id is NULL') // parent exam || single exam
       .leftJoinAndSelect('e.linkedFromExams', 'l')
       .leftJoinAndSelect('e.children', 'e2')
-      .leftJoinAndSelect('e2.examSchedule', 'examSchedule')
+      .leftJoinAndSelect('e2.examSchedule', 'childExamSchedule')
+      .leftJoinAndSelect('e.examSchedule', 'examSchedule')
       .take(100)
       .printSql();
+  }
+
+  findOneWithScheduling(examId: number, organizationId: string) {
+    return (
+      this.examRepository
+        .createQueryBuilder('e')
+        .where('e.id = :examId', { examId })
+        .andWhere('e.organization_id = :organizationId', { organizationId })
+        .leftJoinAndSelect('e.linkedFromExams', 'l')
+        .leftJoinAndSelect('e.children', 'e2')
+        // get scheduling info of child exams (in case current exam is GROUP PARENT)
+        .leftJoinAndSelect('e2.examSchedule', 'childExamSchedule')
+        // get scheduling info of current exam (in case current exam is GROUP CHILD || SINGLE)
+        .leftJoinAndSelect('e.examSchedule', 'examSchedule')
+        .leftJoinAndSelect('examSchedule.roomBooking', 'roomBooking')
+        .printSql()
+        .getOne()
+    );
   }
 }
